a7f59a0e11479f1cee47a6bf6512ab80e62ac6d4
Luis <luismiticanoy0491@gmail.com>
2026-07-12 10:10:31 -0500

    Despliegue2

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -3,9 +3,6 @@ import { API_BASE_URL } from "../config/runtime";
 
 const API = axios.create({
   baseURL: API_BASE_URL
-  //baseURL: "http://localhost:4000"
-  //baseURL: "https://api.impulsaepos.com"
-
 });
 
 // Interceptor para inyectar Token y Tenant ID automáticamente a todas las llamadas
